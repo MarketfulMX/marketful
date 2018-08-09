f7b2c3a7188d21a76c1806ffa6d7d1ad44e69949
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2018-08-09 15:29:45 -0500

    avance1530908

--- admin/class-admin.php
+++ admin/class-admin.php
@@ -1,62 +1,62 @@
 <?php
 
-/*
+/**
  * Archivo: admin-product-edit-form.php
  * Ultima edición : 7 de agosto de 2018
  *
  * @autor: Adolfo Yanes <adolfo@marketful.mx>
  * @autor: Mauricio Alcala <mauricio@marketful.mx> as proyect admin
  * * @author: Javier Urbano <javierurbano11@gmail.com> as contributor
- * * @author: Angel Salazar <salazar.angel.e@gmail.com> 
+ * * @author: Angel Salazar <salazar.angel.e@gmail.com> as contributor
  *
- * @versión: 1.02
+ * @versión: 
  * @link: marketful.mx
  * @package    mkf
  * @subpackage mkf/admin/partials
  *
  */
 
+/**
+ * Descripción General: 
+ * Archivo PHP que tiene la función de hacer la primera llamada a los diferentes elementos 
+ * que se encuentran dentro de si.
+ */
 
-class MKF_Admin {
+/**
+ * @clase PHP MKF_admin
+ * @Atributos:
+ * private $plg_name (Nombre del plugin)
+ * private $plg_small_name (Nombre corto)
+ * private $version (version)
+ * private $dashboard_menu_name ( Nombre a mostrar en el dashboard)
+ * 
+ * @Métodos
+ 
+ *
+ 
+ *
+ 
+ *
 
-  /**
-   * The Name of this plugin.
-   *
-   * @since    1.0.0
-   * @access   private
-   * @var      string    $plg_name    The Name of this plugin.
-   */
+ * 
+ * @función plg_menu(), encargada de indicar que es lo que se realiza 
+ *
+ *
+ *
+ *
+ */
+class MKF_Admin 
+{
+
   private $plg_name;
-
-  /**
-   * The ID of this plugin.
-   *
-   * @since    1.0.0
-   * @access   private
-   * @var      string    $plg_small_name    The ID of this plugin.
-   */
   private $plg_small_name;
-
-  /**
-   * The version of this plugin.
-   *
-   * @since    1.0.0
-   * @access   private
-   * @var      string    $version    The current version of this plugin.
-   */
   private $version;
-
-
   private $dashboard_menu_name;
-
   /**
-   * Initialize the class and set its properties.
-   *
-   * @since    1.0.0
-   * @param      string    $plg_small_name       The name of this plugin.
-   * @param      string    $version    The version of this plugin.
+   * La @función @publica __construct(@string,@string) Inicializala clase y le asigna sus propiedades
    */
-  public function __construct( $plg_small_name = PLUGIN_GSNAME, $version = PLUGIN_GVERSION ) {
+  public function __construct( $plg_small_name = PLUGIN_GSNAME, $version = PLUGIN_GVERSION ) 
+  {
 
     $this->plg_small_name = $plg_small_name;
     $this->version        = $version;
@@ -64,13 +64,13 @@ class MKF_Admin {
     $this->dashboard_menu_name = $this->plg_small_name . "_dashboard";
 
   }
-
   /**
-   * Register the stylesheets for the admin area.
-   *
-   * @since    1.0.0
+   * La @función @publica enqueve_styles() sobre escribe el estilo tomado de wp en caso de que no se 
+   * haya modificado. 
    */
-  public function enqueue_styles() {
+  //Register the stylesheets for the admin area. Since 1.0.0
+  public function enqueue_styles() 
+  {
 
     wp_enqueue_style($this->plg_small_name, plugin_dir_url( __FILE__ ) . 'css/plg-admin.css', array(),$this->version,'all');
     wp_enqueue_style('datatables',"https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.css",array(),'1.10.18','all');
@@ -79,35 +79,37 @@ class MKF_Admin {
     wp_enqueue_style('bootstrap-map',"https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/css/bootstrap.css.map",array(),'4.1.0','all');
     
   }
-
   /**
-   * Register the JavaScript for the admin area.
-   *
-   * @since    1.0.0
+   * La @función @publica enqueve_styles() sobre escribe los 
+   * @Scripts de JavaScript en caso de que no hayan sido modificados, logra que los datos estén disponibles 
+   * para su secuencia de comandos que normalmente solo puede obtener del lado del servidor de WordPress.
    */
-  public function enqueue_scripts() {
-
-    wp_enqueue_script('Popper',"https://unpkg.com/popper.js@1.14.3/dist/umd/popper.min.js", array(), '1.14.3', false );
-    wp_enqueue_script('bootstrap',"https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js",array(),'4.1.0', false );
+  //Register the JavaScript for the admin area. Since 1.0.0 
+  public function enqueue_scripts() 
+  {
+    wp_enqueue_script('Popper',"https://unpkg.com/popper.js@1.14.3/dist/umd/popper.min.js", array(), '1.14.3', false );wp_enqueue_script('bootstrap',"https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js",array(),'4.1.0', false );
     wp_enqueue_script('datatables',"https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.js" , array(), '1.10.18', false );
     wp_enqueue_script('admin_js_bootstrap_hack', plugin_dir_url( __FILE__ ) . 'js/bootstrap-hack.js', false, '1.0.0', false);
     wp_enqueue_script($this->plg_small_name, plugin_dir_url( __FILE__ ) . 'js/plg-admin.js', array('jquery'), $this->version, false);
-
     wp_enqueue_script( 'ajax-script',
         plugins_url( '/js/myjquery.js', __FILE__ ),
         array('jquery')
     );
+    /**
+     * El @Script que a continuación se utiliza la @función wp_create_nonce(@string) que genera un hash con 
+     * esa informacion para despues ser utilizada para validar la información que se envia.
+     */
     $title_nonce = wp_create_nonce( 'title_example' );
     wp_localize_script( 'ajax-script', 'my_ajax_obj', array(
        // 'ajax_url' => admin_url( 'admin-ajax.php' ),
        'ajax_url' => admin_url( 'admin-ajax.php' ),
        'nonce'    => $title_nonce,
     ) );
-
   }
-
-  public function plg_menu() {
-
+    
+  //Add items to menú panel
+  public function plg_menu() 
+  {
     add_menu_page(
       __( $this->plg_small_name, 'textdomain' ),
       __( $this->plg_name, 'textdomain' ),
