83aa4a99afb70399411c4e0c06b70f429d340aea
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2018-08-13 11:59:00 -0500

    avc12001308

--- includes/class-activator.php
+++ includes/class-activator.php
@@ -1,49 +1,78 @@
 <?php
 if ( !class_exists( 'MKF_Activator', false ) ):
 /**
- * Fired during plugin activation
+ * Archivo: class-admin-product-entries.php
+ * Ultima edición : 13 de agosto de 2018
  *
- * This class defines all code necessary to run during the plugin's activation.
+ * @autor: Adolfo Yanes <adolfo@marketful.mx> as master contributor
+ * @autor: Mauricio Alcala <mauricio@marketful.mx> as proyect admin
+ * @author Javier Urbano <javierurbano11@gmail.com> as contributor
+ * @author Angel Salazar <salazar.angel.e@gmail.com> as contributor
  *
- * @link       http://innodite.com
- * @since      1.0.0
- *
+ * @versión: 1.01
+ * @link: marketful.mx
  * @package    mkf
- * @subpackage mkf/includes
- * @author     Javier Urbano <javierurbano11@gmail.com> at Innodite Inc.
- * @author     Angel Salazar <salazar.angel.e@gmail.com> at Innodite Inc.
+ * @subpackage mkf/admin/partials
+ *
  */
 
-class MKF_Activator extends MKF_DBCore {
+/**
+ * Descripción general:
+ * Instala o desisntala el activador dependiendo si esta activado o no.
+ */
+
+
+/**
+ * @Clase MKF_Activator @hereda metodos y atributos de MKF_DBCore
+ * 
+ * @Atributo
+ * Define $instance con valor NULL
+ * 
+ */
+class MKF_Activator extends MKF_DBCore 
+{
 
   protected static $instance = NULL;
-
-  public static function GetInstance() {
-    if ( is_null( self::$instance ) ) {
+  /**
+   * @función getInstance()
+   * Es una función estatica que accede al objeto $instance y busca que sea nulo, 
+   * en cuyo caso creal el objeto.
+   * En caso contrario retorna el objeto.
+   */
+  public static function GetInstance() 
+  {
+    if ( is_null( self::$instance ) ) 
+    {
       self::$instance = new self;
     }
     return self::$instance;
   }
   /**
-   * Short Description. (use period)
-   *
-   * Long Description.
-   *
-   * @since    1.0.0
+   * @función activate()
+   * Se asigna el valor que retorna RunInstall()
    */
-  public function activate() {
+  public function activate() 
+  {
     $this->RunInstall();
   }
-
-  public function deactivate() {
+  /**
+   * @función desactivate()
+   * Se asigna el valor que retorna RunUninstall()
+   */
+  public function deactivate() 
+  {
     $this->RunUninstall();
   }
-
+  /**
+   * @función RunInstall()
+   */
   private function RunInstall() 
   {
     
   }
-
+  /**
+   * @función RunUninstall()
+   */
   private function RunUninstall()
   {
     
