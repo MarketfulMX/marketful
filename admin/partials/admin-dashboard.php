dfff438a4546eb0fe69fc3e01e9dba2bcccacf03
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2018-08-27 13:44:09 -0500

    errorANSI

--- admin/partials/admin-dashboard.php
+++ admin/partials/admin-dashboard.php
@@ -63,70 +63,80 @@
     {
         text-decoration: none;
     }
-    .contenedor div
+    #instalacion, #instrucciones, #preguntas_frecuentes, #soporte
     {
         display: none;
     }
     #dashboard
     {
-        display:inline;
+        display: inline;
         margin-top: 20px;
-        margin-left: 100px;
+        margin: 10px;
+        font-size: 12px;
+        
     }
-        #dashboard p
-        {
-            max-width: 90%;
-            margin-right: auto;
-        }
-        #dashboard li
-        {
-            font-size: 13px;   
-            max-width: 90%;
-            margin-right: auto;
-        } 
+    .maximo
+    {
+        border-color: #C2C3E8;
+        border-width: 1px;
+        border-style: solid;
+        border-radius: 3px;
+    }
     
 </style>
 <div class="head"> <h3> Dashboard </h3> </div>
-
 <ul class="nav nav-tabs tab-superior" id= tab-superior>
-    <li class="nav-item"><a href="#" id="1" class="nav-link active" onclick="clic(1)" >Inicio</a></li>
-    <li class="nav-item"><a href="#" id="2" class="nav-link" onclick="clic(2)" >Instalación</a></li>
-    <li class="nav-item"><a href="#" id="3" class="nav-link" onclick="clic(3)" >Instrucciones</a></li>
-    <li class="nav-item"><a href="#" id="4" class="nav-link" onclick="clic(4)" >Preguntas Frecuentes</a></li>
-    <li class="nav-item"><a href="#" id="5" class="nav-link" onclick="clic(5)" >Soporte</a></li>
-</ul>
-
-<div class="contenedor">
-    <div id="dashboard">
-        <img style="max-width: 30%; margin-left: -131px; margin-top: 20px;"src="https://www.marketful.mx/assets/Logo_marketful-b973bdcabe50755f3a07dc2b2fae41c501eecb4e06756215b6735f4fd5616c81.png">
-        <p>Ponemos a tu alcance una poderosa herramienta tecnológica diseñada para administrar todas tus plataformas de venta en línea y sincronizar la Información de tus publicaciones, órdenes de compra e inventario.</p>
-        <h3>marketful seller center &#160;</h3>
-        <li> Marketful Seller Center Es una poderosa herramienta open source que te ayuda a unir todos tus canales de venta en un solo lugar de la manera más sencilla e intuitiva, lo cual facilitara tu experiencia de venta y lo más importante la de tus clientes. </li>
-        <li> Controla todos tus canales de venta con marketful seller center puedes administrar todas tus cuentas de venta en un solo lugar.</li>
-        <li> Cambios controlados Los cambios dentro de Marketful Seller Center se reflejan en todas tus plataformas.</li>
-        <li> Adiós complicaciones Con Marketful Seller Center se acabaron las horas de modificar productos iguales en plataformas diferentes. </li>
-        <img style="max-width: 90%; margin-right: auto;" src="https://raw.githubusercontent.com/Skepsis-Consulting/wcplugin/df54ec67afd2c73ac1aada2c683ab07e6c1a45ff/Documentacion/img/page-02(3).jpeg?token=Ajnc0XzeluUqr5JuKkQhvLs-1kbsUyP8ks5bjW41wA%3D%3D">
-        <h3> Versión 1.0</h3>
-        <li>Publicaciones Con *Marketful Seller Center *simplicidad es la norma, todos tus productos en un solo lugar y esto se refleja en el poder de publicar tus productos desde un solo lugar y esto se refleje en todos tus canales de venta, con Marketful Seller Center estas en buenas manos.</li>
-        <li>Ordenes Con Marketful Seller Center tus ordenes de envió jamás estuvieron en tan buenas manos. Monitorea, da seguimiento y da soporte a tus clientes desde un solo lugar, podrás monitorear envíos no importa en donde realizaste la venta, tu Seller Center lo tiene bajo control.</li>
-        <li>Preguntas Imagina algo tan sencillo como un centro único de preguntas en donde podrás administrar todos tus canales de comunicación con tus clientes desde una sola pantalla, esto y más te ofrece Marketful Seller Center.</li>
-        <li>Comunicación posventa Dentro de Marketful Seller Center podrás mantener contacto directo con tus clientes después de la venta y sin importar el canal por donde se haya realizado.</li>
-        <img style="max-width: 90%; margin-right: auto;"src="https://raw.githubusercontent.com/Skepsis-Consulting/wcplugin/df54ec67afd2c73ac1aada2c683ab07e6c1a45ff/Documentacion/img/page-01(4).jpeg?token=Ajnc0Tw_tq-2fUpCkVbukToDDkeKeoVfks5bjW8SwA%3D%3D">
-    </div>
-    <div id="instalacion">
-        <p>Contenido de instalacion</p>
-        <img src="https://www.marketful.mx/assets/Logo_marketful-b973bdcabe50755f3a07dc2b2fae41c501eecb4e06756215b6735f4fd5616c81.png">
-    </div>
-    <div id="instrucciones">
-        <p>Contenido de instrucciones</p>
-        <img src="https://www.marketful.mx/assets/Logo_marketful-b973bdcabe50755f3a07dc2b2fae41c501eecb4e06756215b6735f4fd5616c81.png">
-    </div>
-    <div id="preguntas_frecuentes">
-        <p>Contenido de preguntas frecuentes</p>
-        <img src="https://www.marketful.mx/assets/Logo_marketful-b973bdcabe50755f3a07dc2b2fae41c501eecb4e06756215b6735f4fd5616c81.png">
-    </div>
-    <div id="soporte">
-        <p>Contenido de soporte</p>
-        <img src="https://www.marketful.mx/assets/Logo_marketful-b973bdcabe50755f3a07dc2b2fae41c501eecb4e06756215b6735f4fd5616c81.png">
+        <li class="nav-item"><a href="#" id="1" class="nav-link active" onclick="clic(1)" >Inicio</a></li>
+        <li class="nav-item"><a href="#" id="2" class="nav-link" onclick="clic(2)" >Instalación</a></li>
+        <li class="nav-item"><a href="#" id="3" class="nav-link" onclick="clic(3)" >Instrucciones</a></li>
+        <li class="nav-item"><a href="#" id="4" class="nav-link" onclick="clic(4)" >Preguntas Frecuentes</a></li>
+        <li class="nav-item"><a href="#" id="5" class="nav-link" onclick="clic(5)" >Soporte</a></li>
+    </ul>
+<div class="maximo">
+    <div class="contenedor">
+        <div id="dashboard">
+            <div class="row">
+                <div class="col" id=uno>
+                    <img style="max-width: 40%; margin-left: 0px; margin-top:10px;"src="https://www.marketful.mx/assets/Logo_marketful-b973bdcabe50755f3a07dc2b2fae41c501eecb4e06756215b6735f4fd5616c81.png">
+                    <p>Ponemos a tu alcance una poderosa herramienta tecnológica diseñada para administrar todas tus plataformas de venta en línea y sincronizar la Información de tus publicaciones, órdenes de compra e inventario.</p>
+                    <img style="max-width: 100%; margin-right: auto;" src="https://raw.githubusercontent.com/Skepsis-Consulting/wcplugin/df54ec67afd2c73ac1aada2c683ab07e6c1a45ff/Documentacion/img/page-02(3).jpeg?token=Ajnc0XzeluUqr5JuKkQhvLs-1kbsUyP8ks5bjW41wA%3D%3D">
+                </div>
+                <div class="col">
+                    <h3> Versión 1.0</h3>
+                    <li>Publicaciones Con *Marketful Seller Center *simplicidad es la norma, todos tus productos en un solo lugar y esto se refleja en el poder de publicar tus productos desde un solo lugar y esto se refleje en todos tus canales de venta, con Marketful Seller Center estas en buenas manos.</li>
+                    <li>Ordenes Con Marketful Seller Center tus ordenes de envió jamás estuvieron en tan buenas manos. Monitorea, da seguimiento y da soporte a tus clientes desde un solo lugar, podrás monitorear envíos no importa en donde realizaste la venta, tu Seller Center lo tiene bajo control.</li>
+                    <li>Preguntas Imagina algo tan sencillo como un centro único de preguntas en donde podrás administrar todos tus canales de comunicación con tus clientes desde una sola pantalla, esto y más te ofrece Marketful Seller Center.</li>
+                    <li>Comunicación posventa Dentro de Marketful Seller Center podrás mantener contacto directo con tus clientes después de la venta y sin importar el canal por donde se haya realizado.</li>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col" id="dos">
+                    <h3>marketful seller center &#160;</h3>
+                    <li> Marketful Seller Center Es una poderosa herramienta open source que te ayuda a unir todos tus canales de venta en un solo lugar de la manera más sencilla e intuitiva, lo cual facilitara tu experiencia de venta y lo más importante la de tus clientes. </li>
+                    <li> Controla todos tus canales de venta con marketful seller center puedes administrar todas tus cuentas de venta en un solo lugar.</li>
+                    <li> Cambios controlados Los cambios dentro de Marketful Seller Center se reflejan en todas tus plataformas.</li>
+                    <li> Adiós complicaciones Con Marketful Seller Center se acabaron las horas de modificar productos iguales en plataformas diferentes. </li>
+                </div>
+                <div class="col">
+                    <img style="max-width: 100%; margin-right: auto;"src="https://raw.githubusercontent.com/Skepsis-Consulting/wcplugin/df54ec67afd2c73ac1aada2c683ab07e6c1a45ff/Documentacion/img/page-01(4).jpeg?token=Ajnc0Tw_tq-2fUpCkVbukToDDkeKeoVfks5bjW8SwA%3D%3D">
+                </div>
+            </div>
+        </div>
+        <div id="instalacion">
+            <p>Contenido de instalacion</p>
+            <img src="https://www.marketful.mx/assets/Logo_marketful-b973bdcabe50755f3a07dc2b2fae41c501eecb4e06756215b6735f4fd5616c81.png">
+        </div>
+        <div id="instrucciones">
+            <p>Contenido de instrucciones</p>
+            <img src="https://www.marketful.mx/assets/Logo_marketful-b973bdcabe50755f3a07dc2b2fae41c501eecb4e06756215b6735f4fd5616c81.png">
+        </div>
+        <div id="preguntas_frecuentes">
+            <p>Contenido de preguntas frecuentes</p>
+            <img src="https://www.marketful.mx/assets/Logo_marketful-b973bdcabe50755f3a07dc2b2fae41c501eecb4e06756215b6735f4fd5616c81.png">
+        </div>
+        <div id="soporte">
+            <p>Contenido de soporte</p>
+            <img src="https://www.marketful.mx/assets/Logo_marketful-b973bdcabe50755f3a07dc2b2fae41c501eecb4e06756215b6735f4fd5616c81.png">
+        </div>
     </div>
 </div>
