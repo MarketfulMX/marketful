cfbfeb4b032b74e9f8a3f1dc0aa43f3f20e3b5ae
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2018-09-17 10:11:29 -0500

    vista_mensajeria

--- admin/partials/admin-mensajeria.php
+++ admin/partials/admin-mensajeria.php
@@ -33,7 +33,21 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 
 <script type="text/javascript">
-  
+  /**  
+   * @funcion show()
+   * Muestra el textarea con el mensaje que se va a mostrar en cierta situacion.
+   */
+  function show_msj(num)
+  {
+    if($('#a'+num).css('display') == 'none')
+    {
+      $('#a'+num).css('display','block');
+    }
+    else
+    {
+      $('#a'+num).css('display','none');
+    }
+  }
 </script>
 
 <style type="text/css">
@@ -41,11 +55,92 @@
   {
     margin: 20px;
   }
+  .opc_msj
+  {
+    border-color: #dee2e6;
+    border-width: .5px;
+    border-style: solid; 
+    border-radius: 3px;
+    padding: 10px;
+    background-color: white;    
+    margin-right: 2%; margin-bottom: 1%;
+    vertical-align: middle;
+  }
+  .text_msj
+  {
+    border-color: #dee2e6;
+    border-width: .5px;
+    border-style: solid; 
+    border-radius: 0px;
+    padding: 5px;
+    background-color: white;    
+    margin-right: 2%; margin-bottom: 1%;
+  }
+  #a11, #a22, #a33, #a44
+  {
+    display: none;
+  }
+  .boton_msj
+  {
+    background-color:#E2E5C4; 
+    height: 25px; 
+    width: auto; 
+    border-style: solid; 
+    border-color: #7E7F6D; 
+    border-radius: 3px;
+    font-size: 12px;
+    text-decoration: none;
+    cursor: pointer;
+    font-family: sans-serif;
+  }
+    .boton_msj:hover
+    {
+      border-color: #7E7F6D;
+      background-color: #BCBFA3;
+    }
+    .boton_msj:active
+    {
+      border-color: #3F4036;
+      background-color: #BCBFA3;
+    }
+  div h6
+  {
+    cursor: pointer;  
+  }
+
 </style>
 
 <div class="contenedor_msj">
   <div class="titulo_msj">
     <div class="imagen"><?php echo "<img src='{$imgSrc}' > "; /*Se hace echo de la imagen*/?> </div>
     <h3> Mensajeria</h3>
+  </div>
+  <div class="opc_msj" id="1">
+    <h6 onclick="show_msj(11)">Notificar Envio Listo <i class="far fa-plus-square"></i></h6>
+    <div class="text_msj" id="a11">
+      <textarea rows="4" cols="90" name=""> </textarea><br>
+      <button class="boton_msj"> Actualizar Mensaje</button>
+    </div>
+  </div>
+  <div class="opc_msj" id="2">
+    <h6 onclick="show_msj(22)">Mensaje al recibir una orden de compra con envio <i class="far fa-plus-square"></i></h6>
+    <div class="text_msj" id="a22">
+      <textarea rows="4" cols="90" name=""> </textarea><br>
+      <button class="boton_msj"> Actualizar Mensaje</button>
+    </div>
+  </div>
+  <div class="opc_msj" id="3">
+    <h6 onclick="show_msj(33)">Mensaje al recibir una orden de compra con entrega personal <i class="far fa-plus-square"></i></h6>
+    <div class="text_msj" id="a33">
+      <textarea rows="4" cols="90" name=""> </textarea><br>
+      <button class="boton_msj"> Actualizar Mensaje</button>
+    </div>
+  </div>
+  <div class="opc_msj" id="4">
+    <h6 onclick="show_msj(44)">Mensaje cuando la empresa de envio acusa recibo del paquete <i class="far fa-plus-square"></i></h6>
+    <div class="text_msj" id="a44">
+      <textarea rows="4" cols="90" name=""> </textarea><br>
+      <button class="boton_msj"> Actualizar Mensaje</button>
+    </div>
   </div>
 </div>
