d0fa47aa30a80f2b98e4dd72d29b457f835f9910
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2018-09-30 15:09:38 -0500

    preguntas_v1

--- admin/partials/admin-preguntas.php
+++ admin/partials/admin-preguntas.php
@@ -79,76 +79,96 @@
 		padding: 10px;
 		display: grid;
 	    grid-template-columns: 70% 30%;
-	    grid-template-rows: 90% 10%;
+	    grid-template-rows: 100%;
 	}
-		.mensajero
+		.left_side
 		{
-			height: 100%;
-			margin: 5px 20px;
-			padding: 20px 10px;
-			border-color: #7E7F6D; border-width: 1px; border-style: solid; border-radius: 10px 0px 0px 0px;
-			box-shadow: 0 2px 10px rgba(0, 0, 0, 0.25);
-			background-color: #EFF2EF;
-
 			display: grid;
-			grid-template-columns: 100%;
-			grid-template-rows: auto;
-
-			overflow-y: scroll; 
-			max-height: 55vh;
+			grid-template-rows: 90% 10%;
+			grid-template-columns:100%;
 		}
-			.mensaje_r
+			.mensajero
 			{
-				border-color: #878787; border-radius: 40px 0px 40px 40px; border-style: solid; border-width: 1px;
-				background-color: #878787;
-				box-shadow: 0 1px 2px rgba(0,0,0,0.20);
-				color: white; text-shadow: 0px 0px 1px #fff;
-				float: right;
-				padding: 5px 20px 5px 10px;
-				margin-right: 10px;	
-				margin-bottom: 10px;
-				margin-left: 20%;
-				text-align: right;
+				height: 100%;
+				margin: 5px 20px;
+				padding: 20px 10px;
+				border-color: #7E7F6D; border-width: 1px; border-style: solid; border-radius: 3px 0px 0px 0px;
+				box-shadow: 0 2px 10px rgba(0, 0, 0, 0.25);
+				background-color: #FFFFFA;
+
+				display: grid;
+				grid-template-columns: 100%;
+				grid-template-rows: auto;
+
+				overflow-y: scroll; 
+				max-height: 55vh;
 			}
-				.meta_mensaje_r
+				.mensaje_r
 				{
+					border-color: #878787; border-radius: 40px 0px 40px 40px; border-style: solid; border-width: 1px;
+					background-color: #878787;
+					box-shadow: 0 1px 2px rgba(0,0,0,0.20);
+					color: white; text-shadow: 0px 0px 1px #fff;
 					float: right;
-					font-size: 50%; 
+					padding: 5px 20px 5px 10px;
 					margin-right: 10px;	
 					margin-bottom: 10px;
-					margin-left: 80%;
+					margin-left: 20%;
 					text-align: right;
 				}
-			.mensaje_l
-			{
-				border-color: #6E4E6D; border-radius: 0px 40px 40px 40px; border-style: solid; border-width: 1px;
-				background-color: #6E4E6D;
-				box-shadow: 0 1px 2px rgba(0,0,0,0.20);
-				color: white; text-shadow: 0px 0px 1px #fff;
-				float: left;
-				padding: 5px 10px 5px 20px;
-				margin-left: 10px;
-				margin-right: 20%;
-				margin-bottom: 10px;
-				text-align: left;
-			}
-				.meta_mensaje_l
+					.meta_mensaje_r
+					{
+						float: right;
+						font-size: 50%; 
+						margin-right: 10px;	
+						margin-bottom: 10px;
+						margin-left: 80%;
+						text-align: right;
+					}
+				.mensaje_l
 				{
+					border-color: #6E4E6D; border-radius: 0px 40px 40px 40px; border-style: solid; border-width: 1px;
+					background-color: #6E4E6D;
+					box-shadow: 0 1px 2px rgba(0,0,0,0.20);
+					color: white; text-shadow: 0px 0px 1px #fff;
 					float: left;
-					font-size: 50%; 
+					padding: 5px 10px 5px 20px;
 					margin-left: 10px;
-					margin-right: 80%;
+					margin-right: 20%;
 					margin-bottom: 10px;
 					text-align: left;
 				}
+					.meta_mensaje_l
+					{
+						float: left;
+						font-size: 50%; 
+						margin-left: 10px;
+						margin-right: 80%;
+						margin-bottom: 10px;
+						text-align: left;
+					}
+			.nuevo_msj
+			{
+				border-color: #7E7F6D; border-width: 1px; border-style: solid; border-radius: 0px 0px 0px 3px;
+				box-shadow: 0 2px 10px rgba(0, 0, 0, 0.25);
+				background-color: #FFFFFA;
+				height: auto;
+				width: auto;
+				margin: 5px 20px;
+				padding: 0px;
+				padding-top: 4px;
+				padding-left: 4px;
+				display: grid;
+				grid-template-rows: 100%;
+				grid-template-columns: 92% 8%;
+			}
 		.informacion
 		{
 			margin: 5px 10px 5px 5px;
-			border-color: #7E7F6D; border-width: 1px; border-style: solid; border-radius: 0px 10px 10px 0px;
+			border-color: #7E7F6D; border-width: 1px; border-style: solid; border-radius: 0px 3px 3px 0px;
 			box-shadow: 0 2px 10px rgba(0, 0, 0, 0.25);
-			background-color: #EFF2EF;
+			background-color: #FFFFFA;
 			padding: 5px;
-			height: 100%;	
 		}
 			.inf_titulo
 			{
@@ -163,25 +183,11 @@
 				margin: 10px;
 				font-size: 80%;
 			}
-		.nuevo_msj
-		{
-			border-color: #7E7F6D; border-width: 1px; border-style: solid; border-radius: 0px 0px 0px 10px;
-			box-shadow: 0 2px 10px rgba(0, 0, 0, 0.25);
-			background-color: #EFF2EF;
-			height: auto;
-			width: auto;
-			margin: 5px 20px;
-			padding: 0px;
-			padding-top: 4px;
-			padding-left: 4px;
-			display: grid;
-			grid-template-rows: 100%;
-			grid-template-columns: 92% 8%;
-		}
 	.boton
 	{
-		border-color: #6E4E6D;border-radius: 25px 25px 25px 25px; border-style: solid;
+		border-color: #6E4E6D;border-radius: 25px 25px 25px 25px; border-style: solid; border-width: .5px;
 		background-color: #6E4E6D;
+		box-shadow: 0 2px 10px rgba(0, 0, 0, 0.25);
 		color: white;
 		height: 50px;
 		width: 50px;
@@ -193,59 +199,72 @@
 			border-color: #402D3F;
 			cursor: pointer;
 		}
+		.boton:active
+		{
+			border-color: pink;
+		}
 </style>
 <div class="head_ord">
   <div class="imagen"><?php echo "<img src='{$imgSrc}' > "; /*Se hace echo de la imagen*/?></div>
   <h3> Preguntas</h3>
 </div>
 <div class="contenedor">
-	<div class="mensajero">
-
-		<?php 
-		$mensaje = 'Hola este es un mensaje de prueba muy largo para mostrar que el mensajero se ve de manera correcta en esta situacion en la cual los mensajes seran muy largos.';
-		$meta = 'Lunes 20 de Mayo de 2018 13:00:03 ';
-		for($c =0; $c <100; $c++)
-		{
-			if($tipo == 'r')
+	<div class="left_side">
+		<div class="mensajero">
+			<?php 
+			$mensaje = 'Hola este es un mensaje de prueba muy largo para mostrar que el mensajero se ve de manera correcta en esta situacion en la cual los mensajes seran muy largos.';
+			$meta = 'Lunes 20 de Mayo de 2018 13:00:03 ';
+			for($c =0; $c <100; $c++)
 			{
-				$tipo = 'l';
+				if($tipo == 'r')
+				{
+					$tipo = 'l';
+				}
+				else
+				{
+					$tipo = 'r';
+				}
+				echo'
+				<div>
+					<div class="mensaje_'.$tipo.'">
+						'.$mensaje.' 
+					</div>
+					<div class="meta_mensaje_'.$tipo.'">
+						'.$meta.'
+					</div>
+				</div>
+				';
+				echo'
+				<div>
+					<div class="mensaje_'.$tipo.'">
+						'.$mensaje.' 
+					</div>
+					<div class="meta_mensaje_'.$tipo.'">
+						'.$meta.'
+					</div>
+				</div>
+				';
+				echo'
+				<div>
+					<div class="mensaje_'.$tipo.'">
+						:)
+					</div>
+					<div class="meta_mensaje_'.$tipo.'">
+						'.$meta.'
+					</div>
+				</div>
+				';
 			}
-			else
-			{
-				$tipo = 'r';
-			}
-			echo'
+			?>
+		</div>
+		<div class="nuevo_msj">
 			<div>
-				<div class="mensaje_'.$tipo.'">
-					'.$mensaje.' 
-				</div>
-				<div class="meta_mensaje_'.$tipo.'">
-					'.$meta.'
-				</div>
+				<textarea id="textarea"rows="2" cols="94"> </textarea>
 			</div>
-			';
-			echo'
 			<div>
-				<div class="mensaje_'.$tipo.'">
-					'.$mensaje.' 
-				</div>
-				<div class="meta_mensaje_'.$tipo.'">
-					'.$meta.'
-				</div>
+				<button class="boton" onclick="add_msj()"><i class="fas fa-location-arrow"></i></button>
 			</div>
-			';
-			echo'
-			<div>
-				<div class="mensaje_'.$tipo.'">
-					:)
-				</div>
-				<div class="meta_mensaje_'.$tipo.'">
-					'.$meta.'
-				</div>
-			</div>
-			';
-		}
-		?>
+		</div>
 	</div>
 	<div class="informacion">
 		<div class="inf_titulo">
@@ -264,14 +283,6 @@
 			<div class="inf_1">
 				Dato 4
 			</div>
-		</div>
-	</div>
-	<div class="nuevo_msj">
-		<div>
-			<textarea id="textarea"rows="2" cols="94"> </textarea>
-		</div>
-		<div>
-			<button class="boton" onclick="add_msj()"><i class="fas fa-location-arrow"></i></button>
 		</div>
 	</div>
 </div>
