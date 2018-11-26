af942381109c856db440a99a21c02f7d21d735cf
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2018-11-26 12:06:23 -0600

    drop_personal

--- admin/partials/admin-orders.php
+++ admin/partials/admin-orders.php
@@ -553,8 +553,8 @@ a span.description {
 <script>
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function myFunction(id) {
+    document.getElementById("myDropdown" + id).classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
@@ -571,6 +571,8 @@ window.onclick = function(event) {
     }
   }
 }
+
+
 </script>
 
 <!-- Filtros de busqueda. -->
@@ -783,7 +785,7 @@ window.onclick = function(event) {
                 <div class="alinear-derecha">
                   <a href="?page=mkf-product-orders-details&id='.$order->id.'&pid='.$order->item_product_id.'" target="_blank"> Ver Detalles </a>
                   <div class="dropdown">
-                    <button onclick="myFunction()" class="dropbtn" >
+                    <button onclick="myFunction('.$order->id.')" class="dropbtn" >
                       <span class="myml-ui-dropdown-actions__icon" style="pointer-events: none;">
                         <svg width="8" height="14" viewBox="0 0 8 35" xmlns="http://www.w3.org/2000/svg">
                         <title>A9B9EA24-301D-48AB-ADBC-23CE01B1CCE1</title><g fill="#333" fill-rule="evenodd">
@@ -792,7 +794,7 @@ window.onclick = function(event) {
                         </svg>
                       </span>
                     </button>
-                    <div id="myDropdown" class="dropdown-content">
+                    <div id="myDropdown'.$order->id.'" class="dropdown-content">
                       <a href="#home">Cancelar venta</a>
                       <a href="#about">Tengo un problema</a>
                     </div>
@@ -937,7 +939,7 @@ window.onclick = function(event) {
                 <div class="alinear-derecha">
                   <a href="?page=mkf-product-orders-details&id='.$order->id.'&pid='.$order->item_product_id.'" target="_blank"> Ver Detalles </a>
                   <div class="dropdown">
-                    <button onclick="myFunction()" class="dropbtn" >
+                    <button onclick="myFunction('.$order->id.')" class="dropbtn" >
                       <span class="myml-ui-dropdown-actions__icon" style="pointer-events: none;">
                         <svg width="8" height="14" viewBox="0 0 8 35" xmlns="http://www.w3.org/2000/svg">
                         <title>A9B9EA24-301D-48AB-ADBC-23CE01B1CCE1</title><g fill="#333" fill-rule="evenodd">
@@ -946,7 +948,7 @@ window.onclick = function(event) {
                         </svg>
                       </span>
                     </button>
-                    <div id="myDropdown" class="dropdown-content">
+                    <div id="myDropdown'.$order->id.'" class="dropdown-content">
                       <a href="#home">Cancelar venta</a>
                       <a href="#"> Agregar Stock </a>
                       <a href="#"> PReguntas </a>
