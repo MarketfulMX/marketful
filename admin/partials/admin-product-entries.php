0fda3bb4ebe5e59edc5f9598c93eb194a24e1bfe
oggs22 <adolfoyanes@gmail.com>
2018-12-03 10:17:31 -0600

    parentesis

--- admin/partials/admin-product-entries.php
+++ admin/partials/admin-product-entries.php
@@ -205,7 +205,7 @@ $imgSrc   = plugins_url( '../img/Marketful.png', __FILE__ );
             <?php 
 
               $titulo = get_post_meta($product->ID, "titulo_ml", $single = true );
-              if(strlen($titulo < 1)){
+              if(strlen($titulo) < 1){
                   $post_7 = get_post( $product->ID );
                   $titulo = $post_7->post_title;
               }
