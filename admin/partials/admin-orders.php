44dc5409d25271217d4b3381a602d568c380d927
oggs22 <adolfoyanes@gmail.com>
2018-12-04 16:00:16 -0600

    sku

--- admin/partials/admin-orders.php
+++ admin/partials/admin-orders.php
@@ -996,11 +996,12 @@ $(document).ready(function() {
                   }else{
                     echo $item_subtotal.' x '.$item_quantity.' unidad';
                   }
+                  $product = new WC_Product($primer_producto['product_id']);
                   echo'
                     
                   </div>
                   <div class="fr3_2_3">
-                    SKU: '.$primer_producto['name'].'  
+                    SKU: '.$product->get_sku().'  
                   </div>
                 </div>
               </div>
