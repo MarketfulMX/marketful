cf264034394c6c62e90684f3e19b3d93fa8957a5
oggs22 <adolfoyanes@gmail.com>
2018-12-04 15:44:40 -0600

    sintax

--- admin/partials/admin-orders.php
+++ admin/partials/admin-orders.php
@@ -1160,7 +1160,7 @@ $(document).ready(function() {
                     
                   </div>
                   <div class="fr3_2_3">
-                    SKU: '.$primer_producto->get_sku().'  
+                    SKU: '.$primer_producto['name'].'  
                   </div>
                 </div>
               </div>
